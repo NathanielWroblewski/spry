7407def22829a01dfecee35533f8d96c058ee66b
Nathaniel Wroblewski <nathanielwroblewski@gmail.com>
2014-01-13 18:20:36 -0800

    Render runtimes in html document

--- lib/spry.rb
+++ lib/spry.rb
@@ -1,5 +1,24 @@
-require "spry/version"
+require 'yaml'
 
-module Spry
-  # Your code goes here...
+pwd = `pwd`
+`git clone #{ARGV[0]} ~/tmp/spry`
+`cd ~/tmp/spry`
+`git checkout #{ARGV[1]}`
+
+serialized_revisions = `git rev-list --reverse HEAD~4..HEAD`
+revisions = YAML.load(serialized_revisions).split ' '
+times = {}
+
+revisions.each do |revision|
+  `git checkout #{revision}`
+  pair = `rspec perf | grep '^RUNTIME:' | awk '{print $3 "," $2}'`
+  id, time = YAML.load(pair).split ','
+  datum = { revision: revision, runtime: time }
+  times[id] ? times[id] << datum : times[id] = [datum]
 end
+
+`git checkout #{ARGV[1]}`
+`cd #{pwd}`
+`sudo rm -rf ~/tmp/spry`
+
+puts YAML.dump(times)
